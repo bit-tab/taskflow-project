ba5200b5a5cb3ad2f81018de511030d7b963fc19
Mónica Pena López <monica.pena@alu.ceacfp.es>
2026-03-28 18:21:54 +0100

    cambio de estilo en la página

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./app.js"
-  ],
+  content: ["./index.html", "./app.js"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          light: '#6366f1',
+          dark: '#4f46e5',
+          accent: '#ec4899'
+        }
+      },
+      borderRadius: {
+        'cloud': '3rem',
+      }
+    },
   },
   plugins: [],
 }
